09594ae06c1250f7ea53955ea11173e4214c41a8
Michal Ivičič <michaljivicic@gmail.com>
2022-05-17 23:57:40 +0200

    + basic map analysis

--- src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/world/worlds/WaveFunctionCollapsedWorld.kt
+++ src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/world/worlds/WaveFunctionCollapsedWorld.kt
@@ -8,9 +8,30 @@ import cz.cuni.gamedev.nail123.roguelike.world.Area
 import cz.cuni.gamedev.nail123.roguelike.world.builders.wavefunctioncollapse.WFCAreaBuilder
 import org.hexworks.zircon.api.data.Position3D
 
+enum class HelperMapTile {
+    Room, Corridor, Wall
+}
+
 class WaveFunctionCollapsedWorld: DungeonWorld() {
     override fun buildLevel(floor: Int): Area {
         val area = WFCAreaBuilder(GameConfig.AREA_SIZE).create()
+
+        val helperMap = emptyMap<Position3D, HelperMapTile>()
+        val directionBlocked = { x: Position3D -> area.blocks[key + x] }
+
+        area.blocks.forEach { (key, value) ->
+            val directionBlocked = { x: Position3D -> Pathfinding.doorOpening(area.blocks[key + x]) }
+            if(directionBlocked(Position3D.defaultPosition())
+                helperMap[key] = HelperMapTile.Wall
+            if(directionBlocked(Position3D.create(1,0,0)) && directionBlocked(Position3D.create(-1,0,0))
+                || (directionBlocked(Position3D.create(0,1,0)) && directionBlocked(Position3D.create(0,-1,0)))) {
+                helperMap[key] = HelperMapTile.Corridor
+            } else
+                helperMap[ke] = HelperMapTile.Room
+        }
+
+
+
 
         area.addAtEmptyPosition(
             area.player,
