1c5ca88d83a82020bb647833a735c684f3054db1
Michal Ivičič <michaljivicic@gmail.com>
2022-05-18 12:50:14 +0200

    pathfinding done

--- src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/world/worlds/WaveFunctionCollapsedWorld.kt
+++ src/jvmMain/kotlin/cz/cuni/gamedev/nail123/roguelike/world/worlds/WaveFunctionCollapsedWorld.kt
@@ -15,33 +15,33 @@ enum class HelperMapTileType {
 
 class WaveFunctionCollapsedWorld: DungeonWorld() {
     data class HelperMapTile(val type: HelperMapTileType, val index: Int = 0)
+    data class GetSameNeighbouringTilesResult(val tiles: List<Position3D>, val neighboursOfTiles: List<Position3D>)
 
     // First result are all the tiles with same type that can be reached from position by tiles with same type, next result is list of neighbours of the first tiles
-    fun getSameNeighbouringTiles(helperMap: Map<Position3D, HelperMapTile>, position: Position3D): (List<Position3D>, List<Position3D>) {
-        val tileType = helperMap[position].type
+    fun getSameNeighbouringTiles(helperMap: Map<Position3D, HelperMapTile>, position: Position3D): GetSameNeighbouringTilesResult {
+        val tileType = helperMap[position]?.type
         val openVertices = ArrayDeque<Position3D>()
-        openVertices.add(position)
         val visitedVertices = mutableSetOf<Position3D>()
+        openVertices.add(position)
+        visitedVertices.add(position)
         val neighbouringVertices = mutableSetOf<Position3D>()
 
         while (openVertices.isNotEmpty()) {
             val currentPos = openVertices.pollFirst()
-            Pathfinding.fourDirectional(currentPos.position)
-                .filter { helperMap[it].type == tileType && !visitedVertices.contains(it) }
+            Pathfinding.fourDirectional(currentPos)
+                .filter { helperMap[it]?.type == tileType && !visitedVertices.contains(it) }
                 .forEach {
                     openVertices.addLast(it)
                     visitedVertices.add(it)
                 }
-            Pathfinding.fourDirectional(currentPos.position)
-                .filter { helperMap[it].type != tileType }
+            Pathfinding.fourDirectional(currentPos)
+                .filter { helperMap[it] != null && helperMap[it]?.type != tileType }
                 .forEach {
-                    visitedVertices.
+                    neighbouringVertices.add(it)
                 }
         }
-    }
 
-    fun getNeighboursOfSameNeighbouringTiles(helperMap: Map<Position3D, HelperMapTile>, position: Position3D): List<Position3D> {
-
+        return GetSameNeighbouringTilesResult(visitedVertices.toList(), neighbouringVertices.toList())
     }
 
     override fun buildLevel(floor: Int): Area {
@@ -50,10 +50,11 @@ class WaveFunctionCollapsedWorld: DungeonWorld() {
         // Make helper map
         val helperMap = mutableMapOf<Position3D, HelperMapTile>()
         area.blocks.forEach { (key, value) ->
-            val directionBlocked = { x: Position3D -> area.blocks[key + x].let { if (it == null) true else Pathfinding.doorOpening(it) }
-            if(directionBlocked(Position3D.defaultPosition()))
-                helperMap[key] = HelperMapTile (HelperMapTileType.Wall)
-            if(directionBlocked(Position3D.create(1,0,0)) && directionBlocked(Position3D.create(-1,0,0))
+            val directionBlocked = { x: Position3D -> area.blocks[key + x].let { if (it == null) true else Pathfinding.doorOpening(it) } }
+            if(directionBlocked(Position3D.defaultPosition())) {
+                helperMap[key] = HelperMapTile(HelperMapTileType.Wall)
+            }
+            else if(directionBlocked(Position3D.create(1,0,0)) && directionBlocked(Position3D.create(-1,0,0))
                 || (directionBlocked(Position3D.create(0,1,0)) && directionBlocked(Position3D.create(0,-1,0)))) {
                 helperMap[key] = HelperMapTile (HelperMapTileType.Corridor)
             } else
